38012e0d7255b1caaf03d777951e4c025176283c
Sunisima <lsun8525@gmail.com>
2025-09-15 12:37:22 +0200

    Adjusted positions and speed

--- WpfSolarSystem/MainWindow.xaml.cs
+++ WpfSolarSystem/MainWindow.xaml.cs
@@ -1,24 +1,43 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfSolarSystem
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+{ 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            //The positions which to orbit around
+            int x = 370;
+            int y = 225;
+
+            Planets Earth = new Planets(90, 90, 0.04, x, y, 180, Brushes.LimeGreen);
+            this.solarSystemCanvas.Children.Add(Earth.PlanetEllipse);
+            
+            Planets Pluto = new Planets(10, 10, 0.03, x, y, 240, Brushes.DarkRed);
+            this.solarSystemCanvas.Children.Add(Pluto.PlanetEllipse);
+
+            Planets Mercury = new Planets(40, 40, 0.05, x, y, 120, Brushes.DimGray);
+            this.solarSystemCanvas.Children.Add(Mercury.PlanetEllipse);
+
+            //Setting different times for the timer to upload new positions for the planets
+            DispatcherTimer earthTimer = new DispatcherTimer();
+            earthTimer.Interval = TimeSpan.FromMilliseconds(10);
+            earthTimer.Tick += Earth.Move;
+            earthTimer.Start();
+
+            DispatcherTimer plutoTimer = new DispatcherTimer();
+            plutoTimer.Interval = TimeSpan.FromMilliseconds(20);
+            plutoTimer.Tick += Pluto.Move;
+            plutoTimer.Start();
+
+            DispatcherTimer MarcuryTimer = new DispatcherTimer();
+            MarcuryTimer.Interval = TimeSpan.FromMilliseconds(15);
+            MarcuryTimer.Tick += Mercury.Move;
+            MarcuryTimer.Start();
         }
     }
 }
---
+++ WpfSolarSystem/Planets.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfSolarSystem
+{
+    // Class to create our planets and update UI placements for planets
+    public class Planets
+    {
+        public Ellipse PlanetEllipse {get; set;}
+        public double Speed { get; set;}
+        
+        private int _xPos = 0;
+        private int _yPos = 0;
+        private readonly int _orbitRadius = 0;
+
+        //Constructor with parameters to define our plantes later
+        public Planets (int height, int width, double speed, int xPos, int yPos, int orbitRadius, SolidColorBrush color)
+        {
+            PlanetEllipse = new Ellipse()
+            {
+                Height = height,
+                Width = width,
+                Fill = color
+            };
+            this.Speed = speed;       
+            this._orbitRadius = orbitRadius;
+            this._xPos = xPos;
+            this._yPos = yPos;            
+        }
+
+        double testAngle = 0;
+
+        //Updates the plantes movements on the canvas
+        public void Move(Object sender, EventArgs e)
+        {
+            testAngle += this.Speed;
+
+            int newXpos = (int)(Math.Cos(testAngle) * this._orbitRadius + _xPos);
+            int newYpos = (int)(Math.Sin(testAngle) * this._orbitRadius + _yPos);
+
+            if (PlanetEllipse.Parent != null)
+            {
+                Canvas.SetTop(PlanetEllipse, newYpos);
+                Canvas.SetLeft(PlanetEllipse, newXpos);
+            }
+            Console.WriteLine("Planet's move threadID: " + Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
