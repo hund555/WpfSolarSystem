b02abe129cf7b8e2954f06567fa45167cb527a58
hund555 <allanholmiversen@gmail.com>
2025-09-15 12:12:18 +0200

    Added planet class, added hardcoded sun, added planet objects and timers to a list.

--- WpfSolarSystem/MainWindow.xaml.cs
+++ WpfSolarSystem/MainWindow.xaml.cs
@@ -11,14 +11,52 @@ using System.Windows.Shapes;
 
 namespace WpfSolarSystem
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Tuple<Planet, Timer>> planets = new List<Tuple<Planet, Timer>>();
         public MainWindow()
         {
+            int x = 415;
+            int y = 305;
             InitializeComponent();
+            Planet earth = new Planet("Earth", 250, 50, 50, 0.05, x, y, Brushes.Blue);
+            Planet mars = new Planet("Mars", 300, 40, 40, 0.03, x, y, Brushes.Red);
+            Planet mercury = new Planet("Mercury", 125, 25, 25, 0.06, x, y, Brushes.Gray);
+            Planet venus = new Planet("Venus", 175, 35, 35, 0.04, x, y, Brushes.Orange);
+            planets.AddRange
+                (
+                    new Tuple<Planet, Timer>
+                    (
+                        item1: earth, new Timer(MovePlanet, earth, 0, 20)
+                    ),
+                    new Tuple<Planet, Timer>
+                    (
+                        item1: mars, new Timer(MovePlanet, mars, 0, 20)
+                    ),
+                    new Tuple<Planet, Timer>
+                    (
+                        item1: mercury, new Timer(MovePlanet, mercury, 0, 20)
+                    ),
+                    new Tuple<Planet, Timer>
+                    (
+                        item1: venus, new Timer(MovePlanet, venus, 0, 10)
+                    )
+                );
+            foreach (Tuple<Planet, Timer> planet in planets)
+            {
+                solarSystemCanvas.Children.Add(planet.Item1.Ellipse);
+            }
+        }
+
+        public void MovePlanet(object state)
+        {
+            Planet planet = (Planet)state;
+            planet.CalculateNextPlanetPosition();
+
+            this.Dispatcher.Invoke(() =>
+            {
+                planet.MovePlanet();
+            });
         }
     }
 }
---
+++ WpfSolarSystem/Planet.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfSolarSystem
+{
+    public class Planet
+    {
+        public Ellipse Ellipse { get; set; }
+        public string Name { get; set; }
+
+        private readonly int orbitRadius = 0;
+        private double speed = 0;
+        private readonly int xpos = 0;
+        private readonly int ypos = 0;
+
+        private int nextXPos = 0;
+        private int nextYPos = 0;
+
+        private double angle = 0;
+
+        public Planet(string name, int orbitRadius, int height, int width, double speed, int xpos, int ypos, SolidColorBrush color)
+        {
+            Ellipse = new Ellipse()
+            {
+                Height = height,
+                Width = width,
+                Fill = color
+            };
+            this.Name = name;
+            this.orbitRadius = orbitRadius;
+            this.speed = speed;
+            this.xpos = xpos;
+            this.ypos = ypos;
+        }
+
+        public void CalculateNextPlanetPosition()
+        {
+            this.angle += this.speed;
+
+            this.nextXPos = (int)(Math.Cos(angle) * this.orbitRadius + this.xpos);
+            this.nextYPos = (int)(Math.Sin(angle) * this.orbitRadius + this.ypos);
+        }
+
+        public void MovePlanet()
+        {
+            Ellipse.SetValue(Canvas.LeftProperty, (double)nextXPos);
+            Ellipse.SetValue(Canvas.TopProperty, (double)nextYPos);
+        }
+    }
+}
